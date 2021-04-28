02464828b626d4a29bed728901524cb5501a1dd9
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-04-28 05:58:21 -0400

    Added Systemcenter namespace

--- application-typings/lib/index.js
+++ application-typings/lib/index.js
@@ -1,6 +1,7 @@
 define(["require", "exports", "./OpenXDA", "./OpenHistorian", "./SOETools"], function (require, exports, OpenXDA_1, OpenHistorian_1, SOETools_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    exports.OpenXDA = exports.OpenHistorian = exports.SOETools = void 0;
     exports.OpenXDA = OpenXDA_1.default;
     exports.OpenHistorian = OpenHistorian_1.default;
     exports.SOETools = SOETools_1.default;
