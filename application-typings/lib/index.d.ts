a19d690b79b848f21482d778f01b49e56a643809
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-07-26 12:26:18 -0400

    Removed .d.ts and .js Files

--- application-typings/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import OpenXDA from './OpenXDA';
-import OpenHistorian from './OpenHistorian';
-import SOETools from './SOETools';
-import SystemCenter from './SystemCenter';
-import Application from './Application';
-export { SOETools, OpenHistorian, OpenXDA, SystemCenter, Application };
--- application-typings/lib/index.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "./OpenXDA", "./OpenHistorian", "./SOETools", "./SystemCenter"], function (require, exports, OpenXDA_1, OpenHistorian_1, SOETools_1, SystemCenter_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.SystemCenter = exports.OpenXDA = exports.OpenHistorian = exports.SOETools = void 0;
-    exports.OpenXDA = OpenXDA_1.default;
-    exports.OpenHistorian = OpenHistorian_1.default;
-    exports.SOETools = SOETools_1.default;
-    exports.SystemCenter = SystemCenter_1.default;
-});
