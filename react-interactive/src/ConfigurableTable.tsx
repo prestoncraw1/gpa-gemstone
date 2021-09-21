bca3a0fb31aa125b32c7b850ec2963b0bdaaaaae
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-09-21 12:24:34 -0400

    Fixed layout of collumns in ConfigurableTable

--- react-interactive/src/ConfigurableTable.tsx
+++ react-interactive/src/ConfigurableTable.tsx
@@ -66,7 +66,7 @@ export default function ConfigurableTable<T> (props: IProps<T>)  {
   return (
     <>
       <Table
-      cols={[...collumns, { key: 'SettingsCog', label: Wrench , headerStyle: { width: 30, padding: 0 }, rowStyle: { width: 0, padding: 0 } }]}
+      cols={[...collumns, { key: 'SettingsCog', label: Wrench , headerStyle: { width: 30, padding: 0, verticalAlign: 'middle', textAlign: 'right' }, rowStyle: {padding: 0 } }]}
       data={props.data}
       onClick={props.onClick}
       sortKey={props.sortKey}
