802bdb48e2cb85c2d7e957b192289c30178866c8
gcsantos <gcsantos@gridprotectionalliance.org>
2022-05-09 14:50:20 -0400

    Added fix to bottom style legend

--- react-graph/src/Legend.tsx
+++ react-graph/src/Legend.tsx
@@ -36,10 +36,11 @@ function Legend(props: IProps) {
   const context = React.useContext(GraphContext)
   const w = (props.location === 'bottom'? props.graphWidth : props.width);
   const h = (props.location === 'right'? props.graphHeight : props.height);
+  const position = (props.location === 'bottom'? 'absolute' : 'relative')
     return (
-      <div style={{ height: h, width: w, position: 'relative', float:(props.location as any) ,display: 'flex', flexWrap: 'wrap' }}>
+      <div style={{ height: h, width: w, position: position, float:(props.location as any) ,display: 'flex', flexWrap: 'wrap', bottom: 0 }}>
         {[...context.Data.values()].map((series, index) => (series.legend !== undefined ?
-              <div key={index}>
+              <div key={index} style={{width:(props.location === 'bottom' ? w/3: w)}}>
                   {series.legend}
           </div> : null))}
       </div>)
