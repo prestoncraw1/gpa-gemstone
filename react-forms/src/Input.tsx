14359badd1b6d4bfd206e50696c63663c70ac80f
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2022-09-29 10:53:35 -0400

    Fixed issue with changed default in react-forms and republished

--- react-forms/src/Input.tsx
+++ react-forms/src/Input.tsx
@@ -74,7 +74,7 @@ export default function Input<T>(props: IProps<T>) {
   return (
     <div className="form-group">
     {(props.Label !== "") ?
-		<label>{props.Label === null ? props.Field : props.Label} 
+		<label>{props.Label === undefined ? props.Field : props.Label} 
 		{props.Help !== undefined? <div style={{ width: 20, height: 20, borderRadius: '50%', display: 'inline-block', background: '#0D6EFD', marginLeft: 10, textAlign: 'center', fontWeight: 'bold' }} onMouseEnter={() => setShowHelp(true)} onMouseLeave={() => setShowHelp(false)}> ? </div> : null}
 		</label> : null}
 		{props.Help !== undefined? 
